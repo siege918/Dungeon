f07e7c7a6f7e624e62f0edd4bc937b2599ef6d49
siege918 <mebecjalt@gmail.com>
2014-09-21 21:57:32 -0500

    DM view prototyped
    A prototype of the DM interface has been built for testing on the
    console

--- js/room.js
+++ js/room.js
@@ -5,15 +5,21 @@ window.ROOMWIDTH = 25;
 	function() {
 		var room = $('.room');
 		room.each( function (index, value) {
-			for (var x = 0; x < window.ROOMWIDTH; x++)
+			for (var y = 0; y < window.ROOMHEIGHT; y++)
 			{
-				for (var y = 0; y < window.ROOMHEIGHT; y++)
+				for (var x = 0; x < window.ROOMWIDTH; x++)
 				{
 					var cell = $('<div class="cell">');
 					cell.attr('data-x', x);
 					cell.attr('data-y', y);
-					cell.addClass('tile-dungeon-wall');
-					cell.css('background-color', '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6));
+					if (x == 0 || y == 0 || x == window.ROOMWIDTH - 1 || y == window.ROOMHEIGHT - 1)
+					{
+						cell.addClass('tile-dungeon-wall');
+					}
+					else
+					{
+						cell.addClass('tile-dungeon-floor');
+					}
 					$(this).append(cell);
 				}
 			}
